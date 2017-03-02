c1dce1db7686a23f2e79429d75d286ac6e6872da
ElTangoBandito <Volfurious@gmail.com>
2017-03-01 19:37:16 -0800

    History Functionality

--- main.c
+++ main.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <errno.h>
+#include <string.h>
+
+#define MAX_HISTORY 10
+int cur_history_count = 0;
+struct Command {
+        char  command[200];
+        int  length;
+};
+
+// Stuff for the History functionality
+// Command array and stuff.
+struct Command  *enteredCommands[10] = { NULL };
 
 // Main Program Loop
 int my_shell();
@@ -13,7 +24,6 @@ int parseInput(char*);
 
 // Execute Command
 int executeCMD(const char*);
-int
 
 int main(void)
 {
@@ -39,21 +49,111 @@ int my_shell()
 
         exit = read_input(user_in);
 
-	if(exit == -1){
-		if
-	}
-
         //printf("size of user_in: %d\n", (int)sizeof(user_in));
         //printf("%s\n",user_in);
 
+        //History function calls
+
+        //saveCommand(user_in);
+        //printHistory();
+        //readIn();
+        //writeOut();
+
         exit = executeCMD(user_in);
 
-        exit = 1;
+        exit = 0;
     }
 
     free(user_in);
     return exit;
 }
+
+//create  command into struct
+int saveCommand(char *user_in){
+        struct Command *temp;
+        strcpy( temp->command, user_in);
+        temp->length = (int)sizeof(user_in);
+        saveToArray(temp);
+        return 1;
+}
+
+int saveToArray(struct Command *in){
+        int i;
+        int maxed = 0;
+
+        if(enteredCommands[9]){
+                maxed = 1;
+                //printf("Array Maxed");
+        }
+
+        if(maxed == 0){
+                for( i = 0; i < MAX_HISTORY; i = i + 1){
+                        if (!enteredCommands[i]){
+                                enteredCommands[i] = in;
+                                //printf("%d\n", i);
+                                break;
+                        }
+                }
+        }
+        if(maxed == 1){
+                int k = 0;
+                //struct Command *newCommandList[10];
+                for(k = 0; k < MAX_HISTORY - 1; k = k + 1){
+                        enteredCommands[k] = enteredCommands[k + 1];
+                }
+                enteredCommands[MAX_HISTORY - 1] = in;
+                //enteredCommands = newCommandList;
+        }
+        return 1;
+}
+
+int printHistory(){
+        int i;
+        for( i = 0; i < MAX_HISTORY; i = i + 1){
+                if (enteredCommands[i]){
+                        int j = 10 - i;
+                        printf("%d %s\n", j, enteredCommands[i]->command);
+                }
+        }
+        return 1;
+}
+//write to file
+int writeOut(){
+        FILE *outFile;
+
+        outFile = fopen("history.txt", "w+");
+        int i;
+        for( i = 0; i < MAX_HISTORY; i = i + 1){
+                if (enteredCommands[i]){
+                        fputs(enteredCommands[i]->command, outFile);
+                }
+        }
+        //fputs("Testing stuff", outFile);
+        fclose(outFile);
+        return 1;
+}
+//read File needs error checking
+int readIn(){
+        FILE *inFile;
+        inFile = fopen("history.txt", "r");
+        char line[255];
+
+        int i = 0;
+  		if(inFile){
+            while (fgets(line, sizeof(line), inFile)){
+                    struct Command *newCommand;
+                    newCommand = malloc(sizeof(newCommand));
+                    strcpy( newCommand->command, line);
+                    newCommand->length = (int)sizeof(line);
+                    saveToArray(newCommand);
+                    i = i + 1;
+                    //printf("%s", line);
+            }
+        }
+		fclose(inFile);
+        return 1;
+}
+
 
 // Working...
 // Needs Error handling and buffer overflow detection
@@ -70,88 +170,18 @@ int read_input(char *user_in)
 
 int executeCMD(const char * cmd)
 {
-	char * comm = strdup(cmd);
-	char *params[10];
-	int ind = 0;
-
-	params[ind++] = strtok(comm, " ");
-	while(params[ind-1] != NULL){
-		params[ind++] = strtok(NULL, " ");
-	}
-
-	ind--;
-	int dex = 0;
-	if(strcmp(params[ind-1], "&") == 0){
-		dex = 1;
-		params[--ind] = NULL;
-	}
-
-	int io[2] = {-1, -1};
-
-	while(ind >= 3){
-
-		if(strcmp(params[ind-2], ">") == 0){
-			io[1] = open(params[argc-1], 0_CREATE|O_WRONLY|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|SIRGRP|S_IWGRP);
-			if(io[1] == -1){
-				perror("open");
-				free(comm);
-				return;
-			}
-
-			params[argc-2] = NULL;
-			argc -= 2;
-		}else if (strcmp(params[argc-2], "<") == 0) {
-			io[0] = open(params[argc-1], O_RDONLY);
-			if (io[0] == -1){
-				perror("open");
-				free(comm);
-				return;
-			}
-			params[argc-2] = NULL;
-			argc -= 2;
-		} else {
-			break;
-		}
-	}
-			
-
-	int stat;
     printf("EXEC FUNCTION\n");
     printf("%s\b", cmd);
     pid_t pid;
 
     pid = fork();
-	//LINE 82+
+
     if(pid < 0)
         fprintf(stderr, ">> ERROR");
-	perror("fork");
-	break;
-    else if(pid == 0){
-        /*execlp(cmd, cmd, "-la", "/", (char *)NULL);*/
-	if (io[0] != -1) {
-		if(dup2(fd[0], STDIN_FILENO) != STDIN_FILENO){
-			perror("dup2");
-			exit(1);
-		}
-	}
-	if (io[1] != -1){
-		if(dup2(io[1], STDOUT_FILENO) != STDOUT_FILENO){
-			perror("dup2");
-			exit(1);
-		}
-	}
-	execvp(params[0], params);
-	perror("execvp");
-	exit(0);
-    }else {
-	close(io[0]);
-	close(io[1]);
-	
-	if (!background)
-		waitpid(pid, &status, 0);
-	break;
-        /*wait(NULL);*/
+    else if(pid == 0)
+        execlp(cmd, cmd, "-la", "/", (char *)NULL);
+    else
+        wait(NULL);
 
     return 0;
 }
-
